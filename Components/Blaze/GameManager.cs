eea148b79ede74284c965c66e764310c4d313ae2
Justin <justinknguyen11@gmail.com>
2026-03-23 23:16:09 -0600

    Fix OTP slot capacities to properly represent team capacities
    For Online Team Play (mode 3), mSlotCapacities should be [6, 6] representing
    each team's capacity, not [0, 12] which represents a single 12-slot capacity.
    
    This ensures the UI properly displays team capacity information for both home
    and away teams.

--- Components/Blaze/GameManager.cs
+++ Components/Blaze/GameManager.cs
@@ -213,7 +213,7 @@ internal class GameManager : GameManagerBase.Server
                     mPresenceMode = serverGame.ReplicatedGameData.mPresenceMode,
                     mQueueCapacity = serverGame.ReplicatedGameData.mQueueCapacity,
                     mQueueCount = serverGame.ReplicatedGameData.mQueueCapacity,
-                    mSlotCapacities = serverGame.ReplicatedGameData.mSlotCapacities,
+                    mSlotCapacities = gameMode == "3" ? new List<ushort> { 6, 6 } : serverGame.ReplicatedGameData.mSlotCapacities,
                     mTeamCapacity = teamCapacity,
                     mVoipTopology = VoipTopology.VOIP_DISABLED
                 }
